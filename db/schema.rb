20aafdadd5d87348139dca12263eee873c51c3c4
vaibhavahlawat <vaibhav.aka1@gmail.com>
2012-07-18 17:05:40 +0530

    this commit is upto commit ..done..

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120718104207) do
+ActiveRecord::Schema.define(:version => 20120718110335) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id"
@@ -2189,6 +2189,7 @@ ActiveRecord::Schema.define(:version => 20120718104207) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "login"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
